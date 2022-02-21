3c84d51164c81e1f9bb8f8258ffcfe75ee238b3e
alicedaurelio <86382764+alicedaurelio@users.noreply.github.com>
2022-02-21 11:33:05 -0300

    Add files via upload

---
+++ Aula2c.java
@@ -0,0 +1,18 @@
+package aula2c;
+import javax.swing.JOptionPane;
+
+
+public class Aula2c {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    JOptionPane.showMessageDialog(null, "Java visual usando SWING com o prof William");
+    
+    }
+    
+    
+}
