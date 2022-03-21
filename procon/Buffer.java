6bd149b8dd84453575e3f13c004b6ffa07a465dd
alicedaurelio <86382764+alicedaurelio@users.noreply.github.com>
2022-03-21 11:41:28 -0300

    Add files via upload

---
+++ procon/Buffer.java
@@ -0,0 +1,35 @@
+package procon;
+
+public class Buffer {
+    private int conteudo;
+    private boolean disponivel;
+    
+    public synchronized void set(int idProdutor, int valor){
+        while (disponivel == true){
+            try{                                                    // controla a execução se verdade ou falso
+                System.out.println("Produtor #  " + idProdutor + " esperando... " );
+                wait();
+            }catch(Exception e){
+                e.printStackTrace();                // saber qual é o erro
+            }
+        }
+        conteudo = valor;
+        System.out.println("Produtor #  " + idProdutor + " colocou " + conteudo);
+        disponivel = true;
+        notifyAll();
+    }
+     public synchronized int get(int idConsumidor, int valor){
+         while (disponivel == false){
+             try{
+                 System.out.println("Consumidor # " + idConsumidor + " esperando ...");
+                 wait();
+             }catch (Exception e){
+                 e.printStackTrace();
+             }
+        }
+         System.out.println("Consumidor # " + idConsumidor + " consumiu: ");
+         disponivel = false;
+         notifyAll();                   // controle das Tread
+         return conteudo;
+     }
+}
